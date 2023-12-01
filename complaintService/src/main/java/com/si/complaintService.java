29de31a51bd93d87922cabeb3345e4e504d226e8
Mads Kristensen <madsarke@gmail.com>
2023-12-01 11:24:30 +0100

    basic complaint service

---
+++ complaintService/src/main/java/com/si/complaintService.java
@@ -0,0 +1,30 @@
+package com.si;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class complaintService implements JavaDelegate {
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        // Hent complaint-værdien fra processens variabler
+        String complaint = (String) execution.getVariable("klage");
+
+        // Check om complaint er null eller ej
+        if (complaint.length() > 10) {
+            // Lige nu er en "god" klage, som kan løses, over 10 karakterer lang. Burde nok være mere indviklet
+            // logik til at bestemme hvordan klager skal håndteres, men det har vi ikke fået lavet.
+
+            System.out.println("Succesfuld behandling (false)");
+            // Her kan du tilføje yderligere logik eller handlinger, der skal udføres
+            execution.setVariable("isSuccessful", false);
+        } else {
+
+            System.out.println("Mislykket behandling (true)");
+            // Her kan du tilføje yderligere logik eller handlinger, der skal udføres i tilfælde af mislykket behandling
+            execution.setVariable("isSuccessful", true);
+        }
+    }
+}
